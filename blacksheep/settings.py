404e46c4e6390047ae67b844406b20b2e290c3ff
caritodoer <caritodoer@gmail.com>
2016-11-17 21:49:40 -0300

    segunda carga

--- blacksheep/settings.py
+++ blacksheep/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blacksheep.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], #para informar donde estan los templates que vamos a usar
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
